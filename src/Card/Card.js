2fdf7e41e2296365be4e1c90050c40a1c397d35e
aporte <apiotre@gmail.com>
2020-01-22 23:11:22 +0100

    "panel divided, dogs on hover

---
+++ src/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="container">
+        <div className="card golden"></div>
+        <div className="card labrador"></div>
+        <div className="card chespeaker-bay-retriever"></div>
+        <div className="card husky"></div>
+        <div className="card owczarek-niemiecki"></div>
+        <div className="card owczarek-podhalanski"></div>
+        <div className="card border-collie"></div>
+        <div className="card owczarek-szwajcarski"></div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/MainWrapper/MainWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '../Card/Card.js'
+import './MainWrapper.css';
+
+function MainWrapper() {
+  return (
+    <Card></Card>
+  );
+}
+
+export default MainWrapper;
